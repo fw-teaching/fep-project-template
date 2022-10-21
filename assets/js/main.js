185e7a2e9b3bf6308fa80516ccc23a3e83ed854a
Fredrik Welander <fredrik.welander@subsite.fi>
2022-10-21 20:36:27 +0300

    boilerplate done (?)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,3 @@
-/* Site-wide JS i den här filen (t.ex. huvudmenyn) */
+/* Site-wide JS i den här filen (t.ex. huvudmenyn kan finnas här) */
 
 console.log('main.js init'); // För att se att skriptet laddats in
